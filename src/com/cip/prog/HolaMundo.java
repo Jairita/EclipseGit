8acf40e2e8fa7e31f6d7b61d6e120b12446f2a67
Yurena <yufernain@gmail.com>
2021-02-21 22:58:08 +0000

    Se le añade una línea al código

---
+++ src/com/cip/prog/HolaMundo.java
@@ -0,0 +1,12 @@
+package com.cip.prog;
+
+public class HolaMundo {
+	
+	public static void main(String[] args) {
+	
+		//Esto es un comentario!!! Prints "Hello World!" to the terminal window.
+		System.out.println("Hello World!");
+		
+		System.out.println("¡hola mundo!");
+	}
+}
